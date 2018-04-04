e13b905f7c1db52f6ce281a23f38d7b13b032d0e
fancidev <fancidev@gmail.com>
2018-04-05 07:16:12 +0800

    Add problems 8-10.

---
+++ src/p10.cpp
@@ -0,0 +1,31 @@
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
+#include <iostream>
+#include <numeric>
+#include "euler/prime_table.hpp"
+#include "euler.h"
+
+BEGIN_PROBLEM(10, solve_problem_10)
+	PROBLEM_TITLE("Calculate the sum of all the primes below 2,000,000")
+	PROBLEM_ANSWER("142913828922")
+	PROBLEM_DIFFICULTY(1)
+	PROBLEM_FUN_LEVEL(1)
+	PROBLEM_TIME_COMPLEXITY("N*ln(ln(N))")
+	PROBLEM_SPACE_COMPLEXITY("N")
+END_PROBLEM()
+
+static void solve_problem_10()
+{
+#if 0
+	const int n = 10;
+#else
+	const int n = 2000000;
+#endif
+	euler::prime_table<int> primes(n);
+	long long sum = std::accumulate(primes.begin(), primes.end(), 0LL);
+	std::cout << sum << std::endl;
+}
---
+++ src/p9.cpp
@@ -0,0 +1,35 @@
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a b c, for which
+ *
+ *   a^2 + b^2 = c^2.
+ *
+ * For example, 3^2 + 4^2 = 5^2.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+
+#include <iostream>
+#include "euler/integer_triangle.hpp"
+#include "euler.h"
+
+BEGIN_PROBLEM(9, solve_problem_9)
+	PROBLEM_TITLE("Find the unique Pythagorean triplet that sums to 1000")
+	PROBLEM_ANSWER("31875000")
+	PROBLEM_DIFFICULTY(2)
+	PROBLEM_FUN_LEVEL(1)
+	PROBLEM_TIME_COMPLEXITY("N^2")
+	PROBLEM_SPACE_COMPLEXITY("1")
+	PROBLEM_KEYWORDS("pythagorean")
+END_PROBLEM()
+
+static void solve_problem_9()
+{
+	generate_right_triangles<int>(1000, [](int a, int b, int c) {
+		if (1000 % (a+b+c) == 0)
+		{
+			int x = 1000/(a+b+c);
+			std::cout << a*b*c*x*x*x << std::endl;
+		}
+	});
+}
