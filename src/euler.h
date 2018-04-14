2f7700d3b271c0fcdffee0e3d0205b072be9b6ff
fancidev <fancidev@gmail.com>
2018-04-14 14:37:16 +0800

    tidy: euler.h

--- src/euler.h
+++ src/euler.h
@@ -12,26 +12,26 @@ typedef void (*PROBLEM_FUNC) ();
  * Contains information about a solution to a problem.
  *
  * Each solution is identified by the ID of the problem it solves.
- * 
+ *
  * @ingroup Helper
  */
 struct euler_problem_info
 {
-	int id;                      /**< Problem number. */
-	PROBLEM_FUNC routine;        /**< Entry of the solution routine. */
-	const char *title;           /**< Problem title. */
-	int difficulty;              /**< Difficulty of the problem (on a scale of 1 to 3). */
-	int fun_level;               /**< Fun level of the problem (on a scale of 1 to 3). */
-	const char *time_complexity; /**< Time complexity of the solution. */
-	const char *space_complexity;/**< Space complexity of the solution. */
-	const char *answer;          /**< Answer to the problem. */
-	const char *keywords;        /**< Keywords separated by comma. */
+  int id;                      /**< Problem number */
+  PROBLEM_FUNC routine;        /**< Entry point of solution routine */
+  const char *title;           /**< Problem title */
+  int difficulty;              /**< Problem difficulty (on scale of 1 to 3) */
+  int fun_level;               /**< Problem fun level (on scale of 1 to 3) */
+  const char *time_complexity; /**< Time complexity of solution */
+  const char *space_complexity;/**< Space complexity of solution */
+  const char *answer;          /**< Answer to the problem */
+  const char *keywords;        /**< Keywords separated by comma */
 
 public:
-	/// Constructs an empty object.
-	euler_problem_info() : id(0), routine(NULL), title(NULL), difficulty(0),
-		fun_level(0), time_complexity(NULL), space_complexity(NULL),
-		answer(NULL), keywords(NULL) { }
+  /// Constructs an empty object.
+  euler_problem_info() : id(0), routine(), title(), difficulty(0),
+    fun_level(0), time_complexity(), space_complexity(),
+    answer(), keywords() { }
 };
 
 /**
@@ -49,13 +49,13 @@ extern void register_problem(const euler_problem_info &info);
 /// @param _routine Entry point of the solution routine.
 /// @ingroup Helper
 #define BEGIN_PROBLEM(pid, _routine) \
-	static void _routine(); \
-	static int _init_problem_##pid(); \
-	static int _var_problem_##pid = _init_problem_##pid(); \
-	static int _init_problem_##pid() { \
-	euler_problem_info info; \
-	info.id = pid; \
-	info.routine = _routine;
+  static void _routine(); \
+  static int _init_problem_##pid(); \
+  static int _var_problem_##pid = _init_problem_##pid(); \
+  static int _init_problem_##pid() { \
+  euler_problem_info info; \
+  info.id = pid; \
+  info.routine = _routine;
 
 /// Sets the title of the problem.
 /// @ingroup Helper
@@ -73,7 +73,7 @@ extern void register_problem(const euler_problem_info &info);
 /// @ingroup Helper
 #define PROBLEM_FUN_LEVEL(x) info.fun_level = x;
 
-/// Sets the time complexity of the solution. 
+/// Sets the time complexity of the solution.
 /// @ingroup Helper
 #define PROBLEM_TIME_COMPLEXITY(x) info.time_complexity = x;
 
@@ -81,7 +81,7 @@ extern void register_problem(const euler_problem_info &info);
 /// @ingroup Helper
 #define PROBLEM_SPACE_COMPLEXITY(x) info.space_complexity = x;
 
-/// Sets the keywords of the problem. Multiple keywords can be separated by 
+/// Sets the keywords of the problem. Multiple keywords can be separated by
 /// comma.
 /// @ingroup Helper
 #define PROBLEM_KEYWORDS(x) info.keywords = x;
@@ -89,9 +89,8 @@ extern void register_problem(const euler_problem_info &info);
 /// Ends the declaration of a solution.
 /// @ingroup Helper
 #define END_PROBLEM() \
-	register_problem(info); \
-	return 0; \
-	}
-
+  register_problem(info); \
+  return 0; \
+  }
 
 #endif
