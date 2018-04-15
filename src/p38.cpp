5c8cddcfef859ad86b482bff6009a17c5efb8ca4
fancidev <fancidev@gmail.com>
2018-04-15 09:16:06 +0800

    tidy: problem 38

--- src/p38.cpp
+++ src/p38.cpp
@@ -22,42 +22,43 @@
 #include "euler.h"
 
 BEGIN_PROBLEM(38, solve_problem_38)
-	PROBLEM_TITLE("Largest 1-9 pandigital number as concatenated product")
-	PROBLEM_ANSWER("932718654")
-	PROBLEM_DIFFICULTY(1)
-	PROBLEM_FUN_LEVEL(1)
-	PROBLEM_TIME_COMPLEXITY("")
-	PROBLEM_SPACE_COMPLEXITY("")
+  PROBLEM_TITLE("Largest 1-9 pandigital number as concatenated product")
+  PROBLEM_ANSWER("932718654")
+  PROBLEM_DIFFICULTY(1)
+  PROBLEM_FUN_LEVEL(1)
+  PROBLEM_TIME_COMPLEXITY("")
+  PROBLEM_SPACE_COMPLEXITY("")
 END_PROBLEM()
 
 static void solve_problem_38()
 {
-	bool verbose = false;
-	int max_number = 0;
-	std::vector<int> digits;
-	for (int n = 2; n <= 9; n++)
-	{
-		for (int m = 1; ; m++)
-		{
-			// Concatenate the digits of m*(1,2,...,n).
-			digits.clear();
-			for (int k = 1; k <= n; k++)
-			{
-				std::copy(euler::digits<10>(k*m).begin(), euler::digits<10>(k*m).end(), 
-					std::back_inserter(digits));
-			}
-			if (digits.size() > 9)
-			{
-				break;
-			}
-			if (euler::is_pandigital<10>(digits.cbegin(), digits.cend()))
-			{
-				int number = euler::from_digits<10,int>(digits.cbegin(), digits.cend());
-				max_number = std::max(max_number, number);
-				if (verbose)
-					std::cout << number << " = (1.." << n << ") * " << m << std::endl;
-			}
-		}
-	}
-	std::cout << max_number << std::endl;
+  int max_number = 0;
+  std::vector<int> digits;
+  for (int n = 2; n <= 9; n++)
+  {
+    for (int m = 1; ; m++)
+    {
+      // Concatenate the digits of m*(1,2,...,n).
+      digits.clear();
+      for (int k = 1; k <= n; k++)
+      {
+        std::copy(euler::digits<10>(k*m).begin(), euler::digits<10>(k*m).end(), 
+          std::back_inserter(digits));
+      }
+      if (digits.size() > 9)
+      {
+        break;
+      }
+      if (euler::is_pandigital<10>(digits.cbegin(), digits.cend()))
+      {
+        int number = euler::from_digits<10,int>(digits.cbegin(), digits.cend());
+        max_number = std::max(max_number, number);
+        if (verbose())
+        {
+          std::cout << number << " = (1.." << n << ") * " << m << std::endl;
+        }
+      }
+    }
+  }
+  std::cout << max_number << std::endl;
 }
