4a5828cff8669ada07dfeeab13fa1719a0e2f18a
fancidev <fancidev@gmail.com>
2018-05-24 20:49:37 +0800

    Rewrite solution 19 to be portable.
    The original solution calls the C standard library function mktime() to
    determine the weekday of a date. This method has two problems:
    
    (1) mktime() expects the argument in local time and adjusts it before
        filling the weekday. Therefore the behavior depends on the time-zone
        setting of the host system.
    
    (2) Under MSVC, mktime() only supports dates after 1970, while the problem
        deals with dates prior to 1970.
    
    The updated solution uses hand-rolled calendar implementation.

--- src/p19.cpp
+++ src/p19.cpp
@@ -1,10 +1,12 @@
 /**
+ * 1 Jan 1900 was a Monday.
+ *
  * How many Sundays fell on the first of the month during the twentieth
  * century (1 Jan 1901 to 31 Dec 2000)?
  */
 
 #include <iostream>
-#include <ctime>
+#include "euler/datetime.hpp"
 #include "euler.h"
 
 BEGIN_PROBLEM(19, solve_problem_19)
@@ -19,17 +21,13 @@ END_PROBLEM()
 
 static void solve_problem_19()
 {
+  const int sunday = euler::datenum(1899, 12, 31);
   int num_sundays = 0;
   for (int year = 1901; year <= 2000; year++)
   {
     for (int month = 1; month <= 12; month++)
     {
-      std::tm t = {0};
-      t.tm_mday = 1;
-      t.tm_mon = month - 1;
-      t.tm_year = year - 1900;
-      std::mktime(&t);
-      if (t.tm_wday == 0)
+      if ((euler::datenum(year, month, 1) - sunday) % 7 == 0)
       {
         num_sundays++;
       }
